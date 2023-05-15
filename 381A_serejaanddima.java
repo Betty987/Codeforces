9d100866417344bede5bcbdc0172f106b518a730
Bethlehem <93065948+Betty987@users.noreply.github.com>
2023-05-15 10:13:43 +0800

    Update 381A_serejaanddima.java

--- 381A_serejaanddima.java
+++ 381A_serejaanddima.java
@@ -1,1 +1,32 @@
-dhhd
+
+import java.util.Scanner;
+//T:O(n), S:O(n)
+ 
+public class serejaanddima {
+    public static Scanner sc = new Scanner(System.in);
+ 
+    public static void main(String[] args) {
+        int n = sc.nextInt(),leftPos = 0, rightPos = n - 1, serejaSum = 0, dimaSum = 0;
+        int[] record = new int[n];
+        for (int i = 0; i < n; i++) {
+            record[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            if (record[leftPos] >= record[rightPos]) {
+                //if it is sereja's turn
+                if (i % 2 == 0) {
+                    serejaSum += record[leftPos++];
+                } else {
+                    dimaSum += record[leftPos++];
+                }
+            } else {
+                if (i % 2 == 0) {
+                    serejaSum += record[rightPos--];
+                } else {
+                    dimaSum += record[rightPos--];
+                }
+            }
+        }
+        System.out.println(serejaSum + " " + dimaSum);
+    }
+}
